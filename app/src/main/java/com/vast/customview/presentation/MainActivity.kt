84b68b18c3ef2c8732c316804f8b657bdf885668
Valeriy Smirnov <smirnov@atlasbiomed.com>
2022-08-04 10:09:05 +0300

    add widgets realisation

--- app/src/main/java/com/vast/customview/presentation/MainActivity.kt
+++ app/src/main/java/com/vast/customview/presentation/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import com.vast.customview.R
 import com.vast.customview.databinding.ActivityMainBinding
+import com.vast.customview.presentation.healthDiary.HealthDiaryTodayContext
+import com.vast.customview.presentation.healthDiary.HealthDiaryTodayStatus
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -12,5 +14,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val healthDiaryTodayContextCom = HealthDiaryTodayContext(
+            status = HealthDiaryTodayStatus.COMPLETE,
+            mainTitle = "Спасибо!",
+            secondTitle = "Сегодня все заполнено :)",
+            nextDate = "20.07 ПН"
+        )
+
+        binding.healthDiaryDashboardWidgetCom.setData(healthDiaryTodayContextCom)
+
+        val healthDiaryTodayContextReq = HealthDiaryTodayContext(
+            status = HealthDiaryTodayStatus.REQUIRED,
+            mainTitle = "Заполните сегодня опросник дневника самочувствия",
+            nextDate = "20.07 ПН"
+            )
+
+        binding.healthDiaryDashboardWidgetReq.setData(healthDiaryTodayContextReq)
+
+        val healthDiaryTodayContextNotReq = HealthDiaryTodayContext(
+            status = HealthDiaryTodayStatus.NOT_REQUIRED,
+            mainTitle = "Дата заполнения: 20.07 ПН",
+            description = "При необходимости вы можете заполнять опросник ежедневно",
+            nextDate = "20.07 ПН"
+        )
+
+        binding.healthDiaryDashboardWidgetNotReq.setData(healthDiaryTodayContextNotReq)
+
     }
 }
---
+++ app/src/main/java/com/vast/customview/presentation/healthDiary/HealthDiaryTodayContext.kt
@@ -0,0 +1,11 @@
+package com.vast.customview.presentation.healthDiary
+
+data class HealthDiaryTodayContext(
+    val status: HealthDiaryTodayStatus,
+    val mainTitle: String,
+    val secondTitle: String? = null,
+    val description: String? = null,
+    val nextDate: String
+
+) {
+}
